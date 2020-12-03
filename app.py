24a0eaf93dda57c1fa4c4c578522ef8113bc9e21
Tracy Jiang <tracyjiangmx@gmail.com>
2020-12-02 16:27:20 -0800

    part 2

--- app.py
+++ app.py
@@ -13,3 +13,14 @@ def show_story_form():
     """ Shows the Madlib story form  """
 
     return render_template("questions.html", prompts=silly_story.prompts)
+
+
+@app.route("/results")
+def display_story():
+    """ Diplay story on story.html """
+    
+    text = silly_story.generate(request.args)
+    return render_template("story.html", text=text)
+
+    
+

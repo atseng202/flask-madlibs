7fbbff7b2f7bb4761d44cb78b8dc507ac5bbba4f
Alan Tseng <atseng202@gmail.com>
2020-12-02 16:10:42 -0800

    Add route to show form with dynamic html

--- app.py
+++ app.py
@@ -7,3 +7,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.route("/questions")
+def show_story_form():
+    """ Shows the Madlib story form  """
+
+    return render_template("questions.html", prompts=silly_story.prompts)

a06fc9e9b466a8f699a4cdbee4a61e78a914d78f
Alan Tseng <atseng202@gmail.com>
2020-12-02 17:54:10 -0800

    Finish user selecting story

--- app.py
+++ app.py
@@ -12,24 +12,25 @@ debug = DebugToolbarExtension(app)
 def show_story_form():
     """ Shows the Madlib story form  """
 
-    story = stories.get(request.args['story-templates'])
+    story_category = request.args['story-templates']
+    story = stories.get(story_category)
 
-
-    return render_template("questions.html", story_type=, prompts=story.prompts)
-
+    return render_template("questions.html", story_category=story_category, prompts=story.prompts)
 
 @app.route("/results")
 def display_story():
     """ Diplay story on story.html """
     
-    text = silly_story.generate(request.args)
+    story_type = request.args.get('story-type')
+    # can we do jquery here to get data-attributes?
+    text = stories.get(story_type).generate(request.args)
     return render_template("story.html", text=text)
 
     
 @app.route("/")
 def index():
     """ Display home page with stories templates to select from """
-    templates = [silly_story, excited_story]
+   
     return render_template("index.html", templates=stories)
 
 
